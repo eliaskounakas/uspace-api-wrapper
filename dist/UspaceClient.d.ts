be864462a68282babf407dc3c920cd2d94307646
eliaskounakas <elias.kounakas@proton.me>
2024-08-11 12:58:01 +0200

    add types

--- dist/UspaceClient.d.ts
+++ dist/UspaceClient.d.ts
@@ -1,9 +1,10 @@
+import type { CourseData } from "./entities";
 export default class UspaceClient {
     #private;
     constructor(session?: string);
     get getSession(): string;
     set setSession(session: string);
     login(username: string, password: string): Promise<void>;
-    getCourses(year: number, isWinterSemester: boolean): Promise<unknown>;
+    getCourses(year: number, isWinterSemester: boolean): Promise<CourseData[]>;
 }
 //# sourceMappingURL=UspaceClient.d.ts.map
